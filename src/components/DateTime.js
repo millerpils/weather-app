f07ea75f0e8e64486c2dbeed64f7f30236eb7064
Miller <sbrn290@pfslap859648.enterprise.internal.city.ac.uk>
2019-04-01 16:36:33 +0100

    Added DateTime class

---
+++ src/components/DateTime.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+class DateTime extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      months: months,
+      dateInMilliseconds: props.dt * 1000
+    }
+    
+    this.getDate = this.getDate.bind(this)
+  }
+
+  getDate() {
+    return new Date(this.state.dateInMilliseconds)
+  }
+  
+  getYear() {
+    let date = this.getDate()
+    return date.getFullYear()
+  }
+  
+  getDay() {
+    let date = this.getDate()
+    let day = date.getDay()
+    return day
+  }
+  
+  getMonth() {
+    let date = this.getDate()
+    let month = date.getMonth()
+    return months[month]
+  }
+
+  render() {
+    let dateString = this.getDay()+', ' + this.getMonth() + ', ' + this.getYear()
+    return (
+      <div>
+        {dateString}
+      </div>
+    )
+  }
+}
+
+export default DateTime
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,34 +1,10 @@
 import React from 'react'
-
+import DateTime from './DateTime'
 
 function WeatherCard(props) {
-  const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-  let dateInMilliseconds = props.weatherList.dt * 1000
-
-  function getDate() {
-    return new Date(dateInMilliseconds)
-  }
-
-  function getYear() {
-    let date = getDate()
-    return date.getFullYear()
-  }
-
-  function getDay() {
-    let date = getDate()
-    let day = date.getDay()
-    return day
-  }
-
-  function getMonth() {
-    let date = getDate()
-    let month = date.getMonth()
-    return months[month]
-  }
-
   return (
     <div className='card'>
-      { getDay() }, { getMonth() }, { getYear() }
+      <DateTime dt={props.weatherList.dt} />
     </div>
   )
 
